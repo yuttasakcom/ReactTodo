7d6a4dddde99bf5b9ab55996464f2ed39e8d84b2
YoProgrammer <yuttasakcom@gmail.com>
2017-11-04 23:45:19 +0700

    create server run static file

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const publicPath = path.join(__dirname, '..', 'dist')
+
+app.use(express.static(publicPath))
+
+app.listen(3000, () => {
+  console.log('Server is up')
+})
