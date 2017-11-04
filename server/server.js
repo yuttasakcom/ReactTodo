996986936fd941f628c1fd8b759972bf412d20b8
YoProgrammer <yuttasakcom@gmail.com>
2017-11-04 23:51:35 +0700

    setup project for heroku

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const port = process.env.PORT || 3000
 const publicPath = path.join(__dirname, '..', 'dist')
 
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up')
 })
